858a66fa97a08f193dd91c46853fdf9fd82c098c
Mark Ibrahim <mark.ibrahim@ofx.com>
2021-02-17 21:27:16 +1100

    Add Exercise 1

---
+++ Woolies/Woolies.Api/Controllers/ExercisesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Woolies.Api.Models;
+
+namespace Woolies.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        [HttpGet("user")]
+        public UserResponse GetUser()
+        {
+            return new UserResponse
+            {
+                Name = "Mark Ibrahim",
+                Token = new Guid("9395ebb4-ddfd-4585-9f0e-4e97d375bc44")
+            };
+        }
+    }
+}
---
+++ Woolies/Woolies.Api/Models/UserResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Woolies.Api.Models
+{
+    public class UserResponse
+    {
+        public string Name { get; set; }
+        public Guid Token { get; set; }
+    }
+}
